f85c1b5c392198b8fe0c5b9c4791e34724b30843
AndrSaf <andrejs0908@gmail.com>
2024-10-22 19:29:04 +0300

    Название

---
+++ script.js
@@ -0,0 +1,9 @@
+window.onload = function()
+{
+    const name = "Сафонов Андрей Сергеевич";
+    const element = document.querySelector("#name");
+    if (element)
+    {
+        element.textContent = name;
+    }
+}
